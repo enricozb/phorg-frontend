e02c71a5a066883445315f1ff4f962f167961fac
Enrico Borba <enricozb@gmail.com>
2020-09-25 17:12:31 -1000

    import error modal

--- src/components/ImportInfo.tsx
+++ src/components/ImportInfo.tsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import axios from "axios";
 import useSWR from "swr";
 
-import "../css/ImportInfo.css";
-
+import { Modal } from "./modals/Modal";
 import { fetcher } from "../api/requests";
 import { ImportStatus } from "../types";
 import { multiselectPathsDialog } from "../utils/electron";
+import { ReactComponent as WarningSVG } from "../img/warning.svg";
+import "../css/ImportInfo.css";
 
 export function ImportButton() {
+  const [showingErrors, setShowingErrors] = useState(false);
   const { data: status, error } = useSWR<ImportStatus>(
     "/api/import/status",
     fetcher,
@@ -25,14 +27,35 @@ export function ImportButton() {
     return <div />;
   }
 
-  if (status.ongoing) {
+  const hasErrors = status.errors.length > 1;
+
+  if (hasErrors || status.ongoing) {
     return (
-      <div className="progress-bar">
-        <div
-          className="progress-value"
-          style={{ width: `${status.percentage * 100}%` }}
-        ></div>
-      </div>
+      <>
+        {showingErrors && (
+          <ErrorModal
+            errors={status.errors}
+            onRequestHide={() => setShowingErrors(false)}
+          />
+        )}
+        <div className={`progress ${hasErrors ? "error" : ""}`}>
+          {hasErrors && (
+            <WarningSVG
+              className="warning"
+              onClick={() => setShowingErrors(true)}
+            />
+          )}
+          {!hasErrors && status.message && (
+            <p className="message">{status.message}</p>
+          )}
+          <div className={`bar ${hasErrors ? "error" : ""}`}>
+            <div
+              className="value"
+              style={{ width: `${status.percentage * 100}%` }}
+            ></div>
+          </div>
+        </div>
+      </>
     );
   }
 
@@ -48,5 +71,20 @@ export function ImportButton() {
     >
       <p>Import Media</p>
     </button>
+  );
+}
+
+interface ErrorModalProps {
+  errors: string[];
+  onRequestHide: () => void;
+}
+
+function ErrorModal(props: ErrorModalProps) {
+  return (
+    <Modal title="Errors" onRequestHide={props.onRequestHide}>
+      {props.errors.map((error, i) => (
+        <li key={i}>{error}</li>
+      ))}
+    </Modal>
   );
 }
