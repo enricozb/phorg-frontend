59bcf60dada848562017970d06308a2512cda830
Enrico Borba <enricozb@gmail.com>
2020-09-30 15:26:04 -1000

    added complex selection logic, m/esc/shift+<movement keys>

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 import useSWR from "swr";
 
@@ -15,26 +15,7 @@ interface Props {
 
 export function Grid(props: Props) {
   const gridRef = useRef<HTMLDivElement>(null);
-
-  const bindKeys = useMousetrap();
-  const [cursor, setCursor] = useState(0);
-  const [selected, setSelected] = useState<Set<number>>(new Set());
-
-  // 160px are the fixed dimensions of thumbnails. Zoom might break this?
-  const getNumCols = () =>
-    Math.floor(gridRef.current!.getBoundingClientRect().width / 160);
-  const clampCursor = (cursor: number) => Math.max(0, Math.min(30, cursor));
-
-  useEffect(() => {
-    return bindKeys({
-      h: () => setCursor((c) => clampCursor(c - 1)),
-      j: () => setCursor((c) => clampCursor(c + getNumCols())),
-      k: () => setCursor((c) => clampCursor(c - getNumCols())),
-      l: () => setCursor((c) => clampCursor(c + 1)),
-      m: () => setSelected((selected) => selected.symDiff(new Set([cursor]))),
-    });
-  }, [cursor, bindKeys]);
-
+  const { cursor, selected } = useKeys(gridRef);
   const { data: library, error } = useSWR<Library>(
     `/api/libraries/${props.libraryId}`,
     fetcher
@@ -50,6 +31,8 @@ export function Grid(props: Props) {
       </div>
     );
   }
+
+  console.log(selected.size);
 
   // sort media by timestamp (earliest first)
   const mediaEntries = Object.entries(
@@ -69,4 +52,84 @@ export function Grid(props: Props) {
       ))}
     </div>
   );
+}
+
+function useKeys(gridRef: React.RefObject<HTMLDivElement>) {
+  const [anchor, setAnchor] = useState<number | null>(null);
+  const [cursor, setCursor] = useState(0);
+  const [anchorSelection, setAnchorSelection] = useState<Set<number>>(
+    new Set()
+  );
+  const [selected, setSelected] = useState<Set<number>>(new Set());
+
+  const bindKeys = useMousetrap();
+
+  // 160px are the fixed dimensions of thumbnails. Zoom might break this?
+  const getNumCols = useCallback(
+    () => Math.floor(gridRef.current!.getBoundingClientRect().width / 160),
+    [gridRef]
+  );
+  const clampCursor = (cursor: number) => Math.max(0, Math.min(30, cursor));
+
+  const moveCursor: Record<string, (cursor: number) => number> = {
+    h: (c) => clampCursor(c - 1),
+    j: (c) => clampCursor(c + getNumCols()),
+    k: (c) => clampCursor(c - getNumCols()),
+    l: (c) => clampCursor(c + 1),
+  };
+
+  useEffect(() => {
+    const clearAnchorSelection = () => {
+      setAnchor(null);
+      setAnchorSelection(new Set());
+    };
+
+    const flushAnchorSelection = () => {
+      setSelected((selected) => selected.union(anchorSelection));
+      clearAnchorSelection();
+    };
+
+    // for each movement key, we also have shift keys for selection
+    const movementKeyMap = {
+      ...Object.entries(moveCursor).reduce(
+        (keyMap, [key, cursorFunc]) => ({
+          ...keyMap,
+          [key]: () => {
+            setCursor(cursorFunc);
+            flushAnchorSelection();
+          },
+          [`shift+${key}`]: () => {
+            const newCursor = cursorFunc(cursor);
+
+            if (anchor === null) {
+              setAnchor(cursor);
+              setAnchorSelection(Set.range(cursor, newCursor));
+            } else {
+              setAnchorSelection(Set.range(anchor, newCursor));
+            }
+
+            setCursor(newCursor);
+          },
+        }),
+        {}
+      ),
+    };
+
+    const selectionKeyMap = {
+      m: () => {
+        flushAnchorSelection();
+        setSelected((selected) =>
+          selected.union(anchorSelection).symDiff(new Set([cursor]))
+        );
+      },
+      esc: () => {
+        setSelected(new Set());
+        clearAnchorSelection();
+      },
+    };
+
+    return bindKeys({ ...movementKeyMap, ...selectionKeyMap });
+  }, [anchor, cursor, bindKeys, getNumCols]);
+
+  return { cursor, selected: selected.union(anchorSelection) };
 }
--- src/utils/extensions.ts
+++ src/utils/extensions.ts
@@ -1,15 +1,27 @@
 export {};
 
 declare global {
+  interface SetConstructor {
+    range(a: number, b: number): Set<number>;
+  }
+
   interface Set<T> {
     filter(predicate: (el: T) => boolean): Set<T>;
-
-    // (self \ other) union (other \ self)
     symDiff(other: Set<T>): Set<T>;
+    union(other: Set<T>): Set<T>;
   }
 }
 
-Set.prototype.filter = function<T> (predicate: (el: T) => boolean) {
+Set.range = function (start: number, end: number) {
+  const newSet = new Set<number>();
+  const [s, e] = [Math.min(start, end), Math.max(start, end)];
+  for (let i = s; i <= e; i++) {
+    newSet.add(i);
+  }
+  return newSet;
+};
+
+Set.prototype.filter = function <T>(predicate: (el: T) => boolean) {
   const newSet = new Set<T>();
   for (const el of this) {
     if (predicate(el)) {
@@ -19,7 +31,7 @@ Set.prototype.filter = function<T> (predicate: (el: T) => boolean) {
   return newSet;
 };
 
-Set.prototype.symDiff = function<T> (other: Set<T>) {
+Set.prototype.symDiff = function <T>(other: Set<T>) {
   const newSet = new Set<T>();
   for (const el of this) {
     if (!other.has(el)) {
@@ -33,5 +45,12 @@ Set.prototype.symDiff = function<T> (other: Set<T>) {
     }
   }
 
+  return newSet;
+};
+
+Set.prototype.union = function <T>(other: Set<T>) {
+  const newSet = new Set<T>();
+  this.forEach(el => newSet.add(el));
+  other.forEach(el => newSet.add(el));
   return newSet;
 }
