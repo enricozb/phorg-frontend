64258d76fbcbc690b5d0c199fa8dd5b99d27eed0
Enrico Borba <enricozb@gmail.com>
2020-11-26 08:59:16 -1000

    nicer scrollbar

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,23 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// transition scrollbar when no longer scrolling
+let scrollTimeout: ReturnType<typeof setTimeout> | null = null;
+
+function hideScrollbar() {
+  document.body.classList.remove("scrolling")
+  scrollTimeout = null;
+}
+
+window.onscroll = function() {
+  document.body.classList.add("scrolling")
+
+  if (scrollTimeout !== null) {
+    clearTimeout(scrollTimeout)
+  }
+  scrollTimeout = setTimeout(hideScrollbar, 1000)
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
